be2abbb47c084264f2423cb48ec0224df5cd7712
pappijx <ayush.papani123@gmail.com>
2024-04-03 23:37:49 +0530

    new changes

--- root/dist/index.d.ts
+++
@@ -1,7 +0,0 @@
-export type { IRecursiveComponent } from './RecursiveComponentBuilder/RecursiveComponentBuilder';
-export { RecursiveComponentBuilder } from './RecursiveComponentBuilder/RecursiveComponentBuilder';
-export type { IAddItem, IDeleteItem, IUpdateNodeByPath, } from './RecursiveComponentBuilder/RecursiveComponentContext';
-export { RecursiveComponentContext } from './RecursiveComponentBuilder/RecursiveComponentContext';
-export type { IColumn, ITable } from './Table';
-export { Table } from './Table';
-//# sourceMappingURL=index.d.ts.map
